472240f2898380df42c473a99fb811b7322ac8c2
shiqi.li <1009903985@qq.com>
2018-11-30 00:11:24 +0800

    wux

--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -1,5 +1,6 @@
 // pages/search/search.js
-const sliderWidth = 130; 
+const sliderWidth = 130;
+const tabs = ["tab1","tab2"];
 
 Page({
 
@@ -7,79 +8,77 @@ Page({
    * 页面的初始数据
    */
   data: {
-    tabs: ["书籍", "资料"],
-    activeIndex: 0,
-    sliderOffset: 0,
-    sliderLeft: 0
+    current: 'tab1',
+    index:0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    var that = this;
-    wx.getSystemInfo({
-      success: function (res) {
-        that.setData({
-          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
-          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
-        });
-      }
-    });
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
-  }
-  ,tabClick: function (e) {
+  },
+  onTabsChange(e) {
+    const { key } = e.detail
     this.setData({
-      sliderOffset: e.currentTarget.offsetLeft,
-      activeIndex: e.currentTarget.id
-    });
-  }
+      current:key,
+      index:tabs.indexOf(key)
+    })
+  },
+  onSwiperChange(e) {
+    const { current } = e.detail
+    this.setData({
+      current: tabs[current],
+      index:current,
+    })
+  },
 })
