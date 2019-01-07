c35f7a86b8a724fc8ad72512a88b5aef0ca5e529
shiqi.li <1009903985@qq.com>
2019-01-08 00:56:06 +0800

    add: 资源列表页面

---
+++ miniprogram/pages/sourceList/sourceList.js
@@ -0,0 +1,66 @@
+import {
+  $wuxToast,
+  $wuxLoading
+} from '../../component/index';
+
+// miniprogram/pages/bookList/bookList.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [],
+    no: false
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    if (options.tag) {
+      this.getDataByTag(options.tag, 'tag');
+    }
+    if (options.name) {
+      this.getDataByTag(options.name, 'name');
+    }
+  },
+
+  /**
+   * 根据标签查询数据列表
+   */
+  getDataByTag: function(value, searchType) {
+    $wuxLoading().show({
+      text: '正在努力的找',
+    })
+    const db = wx.cloud.database()
+    db.collection('source').where({
+      [searchType]: db.RegExp({
+        regexp: value,
+        options: 'i',
+      })
+    }).get().then(res => {
+      if (res.data.length > 0) {
+        this.setData({
+          list: res.data
+        });
+      } else {
+        this.setData({
+          no: true
+        })
+      };
+      $wuxLoading().hide();
+    }).catch(()=>{
+      $wuxToast().show({
+        type: 'forbidden',
+        duration: 1500,
+        color: '#fff',
+        text: '查找失败，请重试！',
+      })
+      this.setData({
+        no: true
+      })
+      $wuxLoading().hide();
+    })
+  }
+})
